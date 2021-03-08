27c6d4b4c43ce97a945ceca9ee02bec0d2f74c51
Alexandre Hideki Siroma <alexandresiroma@hotmail.com>
2021-03-08 15:45:10 -0300

    Update Head title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -40,6 +40,7 @@ export default class MyDocument extends Document {
           <link rel="shortcut icon" href="favicon.png" type="image/png" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
+          <title>GoMove | Login</title>
         </Head>
         <body>
           <Main />
